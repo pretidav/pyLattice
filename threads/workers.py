da5ab56f8d4e1553a601b7f3dcffa5462f37c726
d.preti <d.preti@almawave.it>
2021-10-07 16:25:34 +0200

    fixed local grid

--- threads/workers.py
+++ threads/workers.py
@@ -15,22 +15,56 @@ class Parallel():
         self.pgrid  = pgrid 
         self.worker = worker
         self.tensor_idx = tensor_idx
-        self.local_grid()
-
-    def recursive_split(self,t,factor,n):
-        nn=n
-        out = [np.split(tt,factor[nn],axis=nn) for tt in t][0] #this is wrong
-        nn+=1
-        if nn<len(factor):
-            out = self.recursive_split(t=out,factor=factor,n=nn)
-        return out
+        self.dim = len(pgrid)
+        self.ptensor_idx = self.local_grid()
 
     def local_grid(self):
-        tmp = np.copy(self.tensor_idx)
-        out =  self.recursive_split(t=[tmp],factor=self.pgrid,n=0)
-        print(out)
+        if self.dim>=1:
+            s1 = np.split(self.tensor_idx, self.pgrid[0], axis=0)
+            out = s1
+        if self.dim>=2:
+            s2 = [np.split(ss, self.pgrid[1], axis=1) for ss in s1]
+            out = s2
+        if self.dim>=3:
+            s3 = []
+            for s in s2 : 
+                s3.append([np.split(ss, self.pgrid[2], axis=2) for ss in s])
+            out = s3
+        if self.dim>=4:
+            s4 = [] 
+            for s in s3: 
+                s4j = []
+                for sj in s:
+                    s4j.append([np.split(ss, self.pgrid[3], axis=3) for ss in sj])
+                s4.append(s4j)
+            out = s4
+        if self.dim>=5: 
+            s5 = [] 
+            for s in s4: 
+                s5j = []
+                for sj in s:
+                    s5jj = []
+                    for sjj in sj:
+                        s5jj.append([np.split(ss, self.pgrid[4], axis=4) for ss in sjj])
+                    s5j.append(s5jj)
+                s5.append(s5j)
+            out = s5
+        if self.dim>=6: 
+            s6 = [] 
+            for s in s5: 
+                s6j = []
+                for sj in s:
+                    s6jj = []
+                    for sjj in sj:
+                        s6jjj = []
+                        for sjjj in sjj:
+                            s6jjj.append([np.split(ss, self.pgrid[5], axis=5) for ss in sjjj])
+                        s6jj.append(s6jjj)
+                    s6j.append(s6jj)
+                s6.append(s6j)
+            out = s6        
 
-    
+        return np.array(out) 
 
 
         
